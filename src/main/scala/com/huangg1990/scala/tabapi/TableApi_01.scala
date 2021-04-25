a9888365070a59394103f953c2aec23012ec0209
huanggang <huanggang@xender.com>
2021-04-25 14:15:16 +0800

    t1

--- src/main/scala/com/huangg1990/scala/tabapi/TableApi_01.scala
+++ src/main/scala/com/huangg1990/scala/tabapi/TableApi_01.scala
@@ -39,10 +39,24 @@ object TableApi_01 {
     bsTableEnv.executeSql(firstTableSQL)
 
 
-    val orders: Table = bsTableEnv.from("t_person")
-    val result = orders
-      .select($"user_id", $"user_name", $"ts")
-    result.toAppendStream[(String, String, Long)].print("t1")
+    //    val orders: Table = bsTableEnv.from("t_person")
+    //    val result = orders
+    //      .select($"user_id", $"user_name", $"ts")
+    //    result.toAppendStream[(String, String, Long)].print("t1")
+
+    val querqySQL = "select user_id,count(user_name) as ct from t_person group by user_id"
+    //    val tableResult=bsTableEnv.sqlQuery(querqySQL).execute()
+    //    tableResult.print()
+
+
+    // execute SELECT statement
+    val tableResult1 = bsTableEnv.executeSql(querqySQL)
+    val it = tableResult1.collect()
+    try while (it.hasNext) {
+      val row = it.next
+      println("user_id="+row.getField(0)+",ct="+ row.getField(1))
+    }
+    finally it.close() // close the iterator to avoid resource leak
 
 
     bsEnv.execute("table api test")
